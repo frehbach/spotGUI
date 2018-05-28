fe3b109d18c9abb1d39cfa66d36fec13e0c6a89c
Frederik Rehbach <frederik.re@gmail.com>
2018-05-28 20:11:04 +0200

    Shortened unnecessary test times

--- tests/testthat/tests/testGenoud4DimRF.R
+++ tests/testthat/tests/testGenoud4DimRF.R
@@ -17,6 +17,6 @@ app$setInputs(evaluateData = "click")
 app$setInputs(proposeNewPoint = "click")
 app$setInputs(evaluateData = "click")
 app$setInputs(selectorXAUTO = "X4")
-Sys.sleep(10)
+Sys.sleep(5)
 app$snapshot(list(output = "resultTable"))
 
