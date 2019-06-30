54291ab97c8aefcdef13b11bff6f4f9bf0082f8f
Frederik Rehbach <frederik.re@gmail.com>
2019-06-30 12:10:06 +0200

    Change to cfdProb dimensions

--- R/testFunctions.R
+++ R/testFunctions.R
@@ -37,7 +37,7 @@ funDDMO <- function(vec, a=1, b=5.1/(4*pi^2), c=5/pi, r=6, s=10, t=1/(8*pi), eva
         x2 <- vec[2] * 0.6 + 123
         term1 <- a * (x2 - b*x1^2 + c*x1 - r)^2
         term2 <- s*(1-t)*cos(x1)
-        y <- term1 + term2 + s - 0.397887  + (77.7777)
+        y <- term1 + term2 + s - 0.397887  + sum(vec)
         return(matrix(y, , 1))
     }else{
         return(res = matrix(apply(vec,1,"funDDMO"), , 1))
